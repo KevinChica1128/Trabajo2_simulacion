f86391c54e691256d033e1d8861925587ed89628
Cesar Saavedra <Andrs917@gmail.com>
2018-05-19 13:21:50 -0500

    Informe final final
    Verificar estos items:
    
    La distribución de probabilidad de X afecta la convergencia.
    
    Los parámetros de la distribución de probabilidad de X afecta la convergencia.

--- trabajo2.R
+++ trabajo2.R
@@ -5,7 +5,7 @@
 #-----------------------------------#
 install.packages("goftest")
 library("goftest")
-#función:
+#funci?n:
 Fun<-function(x,n,k){
   N=length(x)
   medias=rep(0,k)
@@ -23,8 +23,8 @@ Fun<-function(x,n,k){
   cvm.test(medias,"pnorm")
   }
 
-#Distribución Logistica: 
-x<-rlogis(20000,0,1)  #Parámetros 0,1
+#Distribuci?n Logistica: 
+x<-rlogis(20000,0,1)  #Par?metros 0,1
 x11()
 par(mfrow=c(2,2))
 Fun(x,1,5000)
@@ -33,7 +33,7 @@ Fun(x,25,5000)
 Fun(x,50,5000)
 
 
-x1<-rlogis(20000,9,4)  #Parámetros 9,4 
+x1<-rlogis(20000,9,4)  #Par?metros 9,4 
 x11()
 par(mfrow=c(2,2))
 Fun(x1,1,5000)
@@ -41,7 +41,7 @@ Fun(x1,10,5000)
 Fun(x1,25,5000)
 Fun(x1,50,5000)
 
-x2<-rlogis(20000,15,6)  #Parámetros 15,6 
+x2<-rlogis(20000,15,6)  #Par?metros 15,6 
 x11()
 par(mfrow=c(2,2))
 Fun(x2,1,5000)
@@ -49,8 +49,8 @@ Fun(x2,10,5000)
 Fun(x2,25,5000)
 Fun(x2,50,5000)
 
-#Distribución Poisson:
-x3<-rpois(20000,1)  #Parámetro lambda=1
+#Distribuci?n Poisson:
+x3<-rpois(20000,1)  #Par?metro lambda=1
 x11()
 par(mfrow=c(2,2))
 Fun(x3,1,5000)
@@ -58,7 +58,7 @@ Fun(x3,5,5000)
 Fun(x3,10,5000)
 Fun(x3,30,5000)
 
-x4<-rpois(20000,5)  #Parámetros lambda=5 
+x4<-rpois(20000,5)  #Par?metros lambda=5 
 x11()
 par(mfrow=c(2,2))
 Fun(x4,1,5000)
@@ -66,7 +66,7 @@ Fun(x4,5,5000)
 Fun(x4,10,5000)
 Fun(x4,30,5000)
 
-x5<-rpois(20000,10)  #Parámetros lambda=10
+x5<-rpois(20000,10)  #Par?metros lambda=10
 x11()
 par(mfrow=c(2,2))
 Fun(x5,1,5000)
